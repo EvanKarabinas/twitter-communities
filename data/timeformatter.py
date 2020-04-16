6af8e33bd283ce491596d3757020cc43708d9c42
EvanKarabinas <evankarabinas@gmail.com>
2020-04-16 15:59:30 +0300

    First commit - collected some data

---
+++ data/timeformatter.py
@@ -0,0 +1,8 @@
+def format(start, end):
+    elapsed_time = end-start
+    if(elapsed_time < 60):
+        return f"\nRun time : {elapsed_time} s."
+    if(elapsed_time < 60*60):
+        return f"\nRun time : {elapsed_time//60} min , {elapsed_time%60} s."
+
+    return f"\nRun time : {elapsed_time//(60*60)} h , {elapsed_time%(60*60)} min."
