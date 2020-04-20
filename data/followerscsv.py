d7704a6b522ae1371c00b12a3039be7c738bcc0e
EvanKarabinas <evankarabinas@gmail.com>
2020-04-20 19:23:13 +0300

    Csv data files updated to contain enough users

--- data/followerscsv.py
+++ data/followerscsv.py
@@ -68,10 +68,10 @@ start_time = time.time()
 
 field_names = ["followee_id", "followee_name", "follower_id", "follower_name"]
 
-with open(f"{sys.argv[1]}_followers_graph_new.csv", 'w') as followers_csv:
+with open(f"{sys.argv[1]}_followers_graph_large_100_20_20_new.csv", 'w') as followers_csv:
     csv_writer = csv.writer(followers_csv)
     csv_writer.writerow(field_names)
-    expand_graph(3, [200, 20, 10], starting_user, csv_writer, followers_csv)
+    expand_graph(3, [100, 20, 30], starting_user, csv_writer, followers_csv)
 
 end_time = time.time()
 print(tf.format(start_time, end_time))
