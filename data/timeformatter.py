9d711c47d7a1f8f587912ac5ce6fd90a5ee143d2
EvanKarabinas <evankarabinas@gmail.com>
2020-05-07 15:05:27 +0300

    timeformatter.py only displays minutes

--- data/timeformatter.py
+++ data/timeformatter.py
@@ -1,8 +1,9 @@
 def format(start, end):
     elapsed_time = end-start
-    if(elapsed_time < 60):
-        return f"\nRun time : {elapsed_time} s."
-    if(elapsed_time < 60*60):
-        return f"\nRun time : {elapsed_time//60} min , {elapsed_time%60} s."
+    # if(elapsed_time < 60):
+    #     return f"\nRun time : {elapsed_time} s."
+    # if(elapsed_time < 60*60):
+    #     return f"\nRun time : {elapsed_time//60} min , {elapsed_time%60} s."
 
-    return f"\nRun time : {elapsed_time//(60*60)} h , {elapsed_time%(60*60)} min."
+    # return f"\nRun time : {elapsed_time//(60*60)} h , {elapsed_time%(60*60)} min."
+    return f"\nRun time : {round(elapsed_time/60)} min ."
